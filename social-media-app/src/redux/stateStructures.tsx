5c9072a3ca11a6c77cfa5490fc3d1e1d26427077
David Olson <david.earl.olson@gmail.com>
2021-08-04 14:52:47 -0700

    added rough stateStructures and assets

---
+++ social-media-app/src/redux/stateStructures.tsx
@@ -0,0 +1,58 @@
+//SUPPORTING STATE MODELS
+export interface IUser{
+    userID: number;
+    username: string;
+    password: string;
+    firstname: string;
+    middlename: string;
+    lastname: string;
+    userEmail:string;
+    profilePhoto: string;
+    backgroundPhoto: string;
+};
+
+export interface IPost{
+    postId: number;
+    postWrittenContent: string;
+    postImage: string;
+    postOwner: IUser;
+    datePosted: Date;
+    groupId: number;
+};
+
+export interface IComment{
+    commentId: number;
+    commentedPost: IPost;
+    commentContent: string;
+    commentAuthor: IUser;
+};
+
+export interface IReply{
+    replyId: number;
+    replyingToComment: IComment;
+    replyContent: string;
+    replyAuthor: IUser;
+};
+
+export interface IGroup{
+    groupId: number;
+    groupName: string;
+    groupCreator: IUser;
+    groupMembers: IUser[];
+}
+
+export interface IEvent{
+    eventId: number;
+    eventDate: Date;
+    eventName: string;
+    attendingUsers: IUser[];
+    eventStatue: EventStatus;
+}
+
+const
+
+enum EventStatus {
+    Open = "Open",
+    Closed = "Closed",
+  }
+
