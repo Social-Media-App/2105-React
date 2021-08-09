bfdbef17fc74129846064a3dacc4f520598856ec
Conor Kent <conorkent37@outlook.com>
2021-08-09 13:21:49 -0500

    deleted files

--- social-media-app/src/App.tsx
+++ social-media-app/src/App.tsx
@@ -1,12 +1,5 @@
 import React from 'react'
 import './App.css'
-<<<<<<< HEAD
-import { useDispatch } from 'react-redux'
-import { userLogin } from './redux/actions'
-import Button from '@material-ui/core/Button'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import LoginComponent from './components/loginpage/loginpage'
-=======
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from './redux/store'
 import { userLogin } from './redux/actons'
@@ -14,7 +7,6 @@ import Button from '@material-ui/core/Button'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import ProtectedRoute from './components/common/ProtectedRoute'
 import SignUpForm from './components/SignUp/SignUpForm'
->>>>>>> fef15cc04be3d42d0bf60b63a49491eb01d2e5e8
 
 function App () {
   const dispatch = useDispatch()
@@ -35,11 +27,7 @@ function App () {
             <Button variant='contained' color='primary' onClick={handleInputLogin}> Login </Button>
             <Link to="/home">Go To Protected Page</Link>
           </Route>
-<<<<<<< HEAD
-          <Route path='/'>  <LoginComponent/> </Route>
-=======
           <ProtectedRoute path='/home' isAuth={isLoggedIn} component={SignUpForm} /> 
->>>>>>> fef15cc04be3d42d0bf60b63a49491eb01d2e5e8
         </Switch>
       </Router>
     </>
