9d2a6358aa63e6a933d9e3f029760eda7c8b2f92
Matthew Precilio <m.precilio20@gmail.com>
2021-08-11 09:50:49 -0500

    full implementation

--- social-media-app/src/App.tsx
+++ social-media-app/src/App.tsx
@@ -1,33 +1,27 @@
 import React from 'react'
 import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from './redux/store'
+import { useDispatch } from 'react-redux'
 import { userLogin } from './redux/actons'
 import Button from '@material-ui/core/Button'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import ProtectedRoute from './components/common/ProtectedRoute'
-import SignUpForm from './components/SignUp/SignUpForm'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ResetPass from './components/reset-pass/reset-pass'
 
 function App () {
   const dispatch = useDispatch()
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
-
-
 
   // Boilerplate to test redux store on a
-  const handleInputLogin = () => {
-    dispatch(userLogin("",""))
+  const handleInput = () => {
+    dispatch(userLogin('david', 'password'))
   }
 
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path='/'>
-            <Button variant='contained' color='primary' onClick={handleInputLogin}> Login </Button>
-            <Link to="/home">Go To Protected Page</Link>
+          <Route path='/reset-pass'>
+            <ResetPass />
           </Route>
-          <ProtectedRoute path='/home' isAuth={isLoggedIn} component={SignUpForm} /> 
+          <Route path='/'> <h1>Hello World</h1> </Route>
         </Switch>
       </Router>
     </>
