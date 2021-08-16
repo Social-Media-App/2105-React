a6856bca4e744a1d3acd9716c057f20a0bb544a0
David Olson <david.earl.olson@gmail.com>
2021-08-16 15:32:57 -0700

    got cors error figured out and post syntax

--- social-media-app/src/components/HomePage/homepage.tsx
+++ social-media-app/src/components/HomePage/homepage.tsx
@@ -5,6 +5,8 @@ import { IPost, IUser } from "../../redux/stateStructures";
 import Masonry from "react-masonry-css";
 import PostContainer from "../common/PostContainer";
 import Snackbar from "../common/snackbar";
+import { useSelector } from "react-redux"
+import {RootState} from '../../redux/store';
 
 const user: IUser = {
     userId: 1,
@@ -16,78 +18,6 @@ const user: IUser = {
     userEmail: "string",
     profilePhoto: "string",
     backgroundPhoto: "string",
-};
-
-const post: IPost = {
-    postId: 1,
-    postWrittenContent: "content",
-    postImage: "angry dog.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post2: IPost = {
-    postId: 2,
-    postWrittenContent: "content",
-    postImage: "angry dog.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post4: IPost = {
-    postId: 4,
-    postWrittenContent: "content",
-    postImage: "cat8.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post5: IPost = {
-    postId: 5,
-    postWrittenContent: "content",
-    postImage: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post6: IPost = {
-    postId: 6,
-    postWrittenContent: "content",
-    postImage: "cat2.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post7: IPost = {
-    postId: 7,
-    postWrittenContent: "content",
-    postImage: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post8: IPost = {
-    postId: 8,
-    postWrittenContent: "content",
-    postImage: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post3: IPost = {
-    postId: 3,
-    postWrittenContent: "content",
-    postImage: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
 };
 
 const breakpointColumnsObj5 = {
@@ -105,13 +35,20 @@ const breakpointColumnsObj = {
 };
 
 function HomePage() {
-    const postList = [post, post2, post3, post4, post5, post6, post7, post8];
+    const postList = useSelector((state:RootState) => state.posts.posts)
+    
+    // useEffect(() => {
+    //     dispatch(getAllPosts());
+    // }, [dispatch]);
+
+
+
     return (
         <>
             <Snackbar />
             <Grid container justifyContent="center" alignItems="flex-start">
                 <Grid item xs={8}>
-                    <PostContainer {...postList} />
+                    <PostContainer postList={postList} />
                 </Grid>
             </Grid>
             <CreatePostIcon ></CreatePostIcon>
