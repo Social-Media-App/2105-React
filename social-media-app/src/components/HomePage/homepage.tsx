57156731b0af6adade2cf9da3f1cb0d0545da833
Conor Kent <conorkent37@outlook.com>
2021-08-17 15:51:32 -0500

    create comment working

--- social-media-app/src/components/HomePage/homepage.tsx
+++ social-media-app/src/components/HomePage/homepage.tsx
@@ -5,89 +5,21 @@ import { IPost, IUser } from "../../redux/stateStructures";
 import Masonry from "react-masonry-css";
 import PostContainer from "../common/PostContainer";
 import Snackbar from "../common/snackbar";
+import { useSelector, useDispatch } from "react-redux"
+import {RootState} from '../../redux/store';
+import { useEffect } from 'react'
+import { getAllPosts } from '../../redux/actons'
 
 const user: IUser = {
     userId: 1,
     username: "string",
     password: "string",
-    firstname: "string",
-    middlename: "string",
-    lastname: "string",
-    userEmail: "string",
-    profilePhoto: "string",
-    backgroundPhoto: "string",
-};
-
-const post: IPost = {
-    postId: 1,
-    content: "content",
-    picture: "angry dog.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post2: IPost = {
-    postId: 2,
-    content: "content",
-    picture: "angry dog.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post4: IPost = {
-    postId: 4,
-    content: "content",
-    picture: "cat8.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post5: IPost = {
-    postId: 5,
-    content: "content",
-    picture: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post6: IPost = {
-    postId: 6,
-    content: "content",
-    picture: "cat2.jpg",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post7: IPost = {
-    postId: 7,
-    content: "content",
-    picture: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post8: IPost = {
-    postId: 8,
-    content: "content",
-    picture: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
-};
-
-const post3: IPost = {
-    postId: 3,
-    content: "content",
-    picture: "",
-    postOwner: user,
-    datePosted: new Date(),
-    groupId: 0,
+    firstName: "string",
+    middleName: "string",
+    lastName: "string",
+    email: "string",
+    profilePicture: "string",
+    backgroundPicture: "string",
 };
 
 const breakpointColumnsObj5 = {
@@ -105,15 +37,23 @@ const breakpointColumnsObj = {
 };
 
 function HomePage() {
-    const postList = [post, post2, post3, post4, post5, post6, post7, post8];
+    const dispatch = useDispatch();
+    const postList = useSelector((state:RootState) => state.posts.posts)
+    
+    useEffect(() => {
+        dispatch(getAllPosts());
+    }, [dispatch]);
+    
+
     return (
         <>
             <Snackbar />
             <Grid container justifyContent="center" alignItems="flex-start">
                 <Grid item xs={8}>
-                    <PostContainer {...postList} />
+                    <PostContainer postList={postList} />
                 </Grid>
             </Grid>
+            <CreatePostIcon ></CreatePostIcon>
         </>
     );
 }
